41afd76385b5e74255b6716d2a0ba4fe970a057b
Naputriz <naputrizwastaken@gmail.com>
2025-05-16 20:20:07 +0700

    Penambahan pertanyaan untuk survei mandiri dan indikasi jika air layak digunakan atau tidak. Tolong cek kembali jika ada pertanyaan lain yang ingin ditanya dan skor bobot untuk setiap pertanyaan, dan juga skor yang dibutuhkan untuk mengindikasi jika air layak digunakan atau tidak

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+// Struct untuk pertanyaan survei mandiri
+typedef struct{
+	char teks[100];
+	int bobotRisiko;
+	int jawaban;
+} pertanyaan;
+
 
 void membuatBox(char teks);
 void startup();
@@ -38,12 +47,42 @@ void startup() {
 }
 
 void surveiMandiri() {
-	int i, input = 0;
+	pertanyaan pertanyaanSurvei[] = { // Daftar pertanyaan
+		{"Apakah air terlihat keruh?", 5, 0},
+		{"Apakah air berbau tidak sedap?", 4, 0},
+		{"Apakah air berasa aneh ketika diminum (misal asin/pahit)?", 3, 0},
+		{"Apakah anda menggunakan filter air?", -1, 0},
+		{"Apakah anda pernah mengalami gatal/iritasi setelah menggunakan air ini?", 3, 0},
+		{"Apakah anda pernah menemu hal tidak wajar dalam air (Misal kotoran atau serangga)?", 2, 0},
+	};
+	int i, input = 0, skor = 0;
+	int n = sizeof(pertanyaanSurvei) / sizeof(pertanyaan); // Menghitung jumlah pertanyaan
+
 	for (i = 0; i < 51; i++) printf ("-");
 	printf ("\n|%18sBijak Air 1.0%18s|\n", "", "");
 	printf ("|%18sSurvei Mandiri%17s|\n", "", "");
 	for (i = 0; i < 51; i++) printf ("-");
+
+	// Menampilkan pertanyaan survei
+	printf("\nJawab pertanyaan berikut dengan 1 (Ya) atau 0 (Tidak)\n");
+
+    for (int i = 0; i < n; i++) {
+        printf("%d. %s ", i + 1, pertanyaanSurvei[i].teks);
+        scanf("%d", &pertanyaanSurvei[i].jawaban);
+        if (pertanyaanSurvei[i].jawaban == 1) {
+            skor += pertanyaanSurvei[i].bobotRisiko;
+        }
+    }
+	// Mendisplay hasil survei
+    printf("\nHasil Penilaian:\n");
+	printf("Skor Kelayakan Air: %d\n", skor);
+    if (skor >= 5){
+		printf("Air tidak layak digunakan. \n");
+	} else {
+		printf("Air layak digunakan.\n");
+	}
 }
+
 
 void kalkulatorAir() {
 	int i, input = 0;
