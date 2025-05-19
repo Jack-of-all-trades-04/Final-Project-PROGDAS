746197b6281e77701bdd811aea99eb03585460ab
Naputriz <naputrizwastaken@gmail.com>
2025-05-19 09:49:57 +0700

    merubah petanyaan agar lebih sesuai dengan flowchart program

--- main.c
+++ main.c
@@ -2,14 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Struct untuk pertanyaan survei mandiri
-typedef struct{
-	char teks[100];
-	int bobotRisiko;
-	int jawaban;
-} pertanyaan;
-
-
 void membuatBox(char teks);
 void startup();
 void surveiMandiri();
@@ -47,41 +39,57 @@ void startup() {
 }
 
 void surveiMandiri() {
-	pertanyaan pertanyaanSurvei[] = { // Daftar pertanyaan
-		{"Apakah air terlihat keruh?", 5, 0},
-		{"Apakah air berbau tidak sedap?", 4, 0},
-		{"Apakah air berasa aneh ketika diminum (misal asin/pahit)?", 3, 0},
-		{"Apakah anda menggunakan filter air?", -1, 0},
-		{"Apakah anda pernah mengalami gatal/iritasi setelah menggunakan air ini?", 3, 0},
-		{"Apakah anda pernah menemu hal tidak wajar dalam air (Misal kotoran atau serangga)?", 2, 0},
-	};
-	int i, input = 0, skor = 0;
-	int n = sizeof(pertanyaanSurvei) / sizeof(pertanyaan); // Menghitung jumlah pertanyaan
+    float ph, kekeruhan, warna; 
+    int bau, rasa, endapan, coliform, skor = 0;
 
-	for (i = 0; i < 51; i++) printf ("-");
-	printf ("\n|%18sBijak Air 1.0%18s|\n", "", "");
-	printf ("|%18sSurvei Mandiri%17s|\n", "", "");
-	for (i = 0; i < 51; i++) printf ("-");
+    for (int i = 0; i < 51; i++) printf ("-");
+    printf ("\n|%18sBijak Air 1.0%18s|\n", "", "");
+    printf ("|%18sSurvei Mandiri%17s|\n", "", "");
+    for (int i = 0; i < 51; i++) printf ("-");
 
-	// Menampilkan pertanyaan survei
-	printf("\nJawab pertanyaan berikut dengan 1 (Ya) atau 0 (Tidak)\n");
-
-    for (int i = 0; i < n; i++) {
-        printf("%d. %s ", i + 1, pertanyaanSurvei[i].teks);
-        scanf("%d", &pertanyaanSurvei[i].jawaban);
-        if (pertanyaanSurvei[i].jawaban == 1) {
-            skor += pertanyaanSurvei[i].bobotRisiko;
-        }
-    }
-	// Mendisplay hasil survei
-    printf("\nHasil Penilaian:\n");
-	printf("Skor Kelayakan Air: %d\n", skor);
-    if (skor >= 5){
-		printf("Air tidak layak digunakan. \n");
-	} else {
-		printf("Air layak digunakan.\n");
+    printf("\nJawablah pertanyaan-pertanyaan berikut\n");
+    printf("1. Berapa nilai pH air?: "); 
+    scanf("%f", &ph);
+    if (ph < 6.5 || ph > 8.5){
+		skor += 5;
+	} 
+    printf("2. Berapa nilai kekeruhan air dalam NTU?: ");
+    scanf("%f", &kekeruhan);
+    if (kekeruhan > 5) {
+		skor += 5;
 	}
+    printf("3. Berapa skala warna air dalam TCU?: ");
+    scanf("%f", &warna);
+    if (warna > 50){
+		skor += 5;
+	}
+    printf("4. Berapa skor kebauan air? (0–5): ");
+    scanf("%d", &bau);
+    if (bau >= 0 && bau <= 5){
+		skor += bau;
+	}
+    printf("5. Berapa rasa tidak sedap air? (0–5): ");
+    scanf("%d", &rasa);
+    if (rasa >= 0 && rasa <= 5) {
+		skor += rasa;
+	}
+    printf("6. Berapa skor endapan dalam air? (0–5): ");
+    scanf("%d", &endapan);
+    if (endapan >= 0 && endapan <= 5){
+		skor += endapan;
+	}
+    printf("7. Berapa jumlah bakteri coliform (dalam CFU): ");
+    scanf("%d", &coliform);
+    if (coliform > 50) {
+		skor += 5;
+	}
+
+    // Tampilkan hasil dan kembali ke menu startup
+    printf("\nHasil Penilaian:\n");
+    printf("Skor Kelayakan Air: %d\n", skor);
+    startup();
 }
+
 
 
 void kalkulatorAir() {
